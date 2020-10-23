791792bd3279b467c67c3f7e39e36f1231f533b2
Duplicated <roc75258@hotmail.com>
2020-10-24 00:49:06 +0700

    [MISC] added header validation middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import bodyParser from 'body-parser';
+import { invalidAuth, rateLimit } from './services/middleware';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
-});
- 
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
-});
+app.use(bodyParser.json());
+app.post('*', [invalidAuth, rateLimit]);
  
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
---
+++ src/services/middleware.js
@@ -0,0 +1,12 @@
+export const invalidAuth = (req, res, next) => {
+  if (!req.header('x-user-id')) {
+    return res
+      .status(401)
+      .send({ successful: false, message: 'Missing user id' });
+  }
+  next();
+};
+
+export const rateLimit = (req, res, next) => {
+  return res.send({ successful: true, message: 'Received POST request' });
+};
