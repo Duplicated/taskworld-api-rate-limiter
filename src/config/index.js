e96928b5bc9296e30f36ef4d85e86509e9885711
Duplicated <roc75258@hotmail.com>
2020-10-24 20:29:09 +0700

    [RATELIMIT] naive impl. to be fixed

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 export default {
   hostName: process.env.HOSTNAME || 'localhost',
   port: process.env.PORT || 8081,
+  requestLimit: process.env.REQUEST_LIMIT || 10,
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || '6379',
--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,12 +1,16 @@
+import config from '../config';
 import redis from './redis';
 
-export const invalidAuth = async (req, res, next) => 
+export const invalidAuth = async (req, res, next) =>
   new Promise(async (resolve, reject) => {
     try {
       if (!req.get('x-user-id')) {
-        resolve(res.status(401).send({ successful: false, message: 'Missing user id' }));
-      }
-      else {
+        resolve(
+          res
+            .status(401)
+            .send({ successful: false, message: 'Missing user id' })
+        );
+      } else {
         resolve(next());
       }
     } catch (error) {
@@ -14,8 +18,26 @@ export const invalidAuth = async (req, res, next) =>
     }
   });
 
+const validateLimit = async (req) => {
+  const userId = req.get('x-user-id');
+  // increment request count for this userId
+  const res = await redis.incr(userId);
+  if (res > config.requestLimit) {
+    return true;
+  }
+  await redis.expire(userId, 60);
+};
 
 export const rateLimit = async (req, res, next) => {
-  await redis.set("foo", "bar");
-  return res.send({ successful: true, message: 'Received POST request' });
+  const status = await validateLimit(req);
+  if (status) {
+    return res
+      .status(429)
+      .send({
+        successful: false,
+        message: 'Too many requests, please try again later',
+      });
+  }
+  return res.status(200).send({ successful: true, message: 'forwarding...' });
+  // forward to reverse proxy or load balancer endpoint
 };
